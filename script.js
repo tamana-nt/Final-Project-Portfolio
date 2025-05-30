5a564554276586ab1628013b8588953ee8cd2893
tamana-nt <tamanabnasrat@gmail.com>
2025-05-30 03:37:33 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    alert('Welcome to my portfolio!');
+};
+
+
+document.getElementById('about').onclick = function() {
+    document.getElementById('about').style.backgroundColor = '#e0e0e0';
+};
